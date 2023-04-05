0ea994df1611527fcddd3b4ce805c29091bc7662
Marco Stoffel <marco.stoffel@softwareag.com>
2023-04-05 13:36:19 +0200

    first commit

---
+++ widget/extended-alarm-widget-plugin.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WidgetPluginComponent } from './extended-alarm-widget-plugin.component';
+import { WidgetPluginConfig } from './extended-alarm-widget-plugin-config.component';
+import {
+  HOOK_COMPONENTS,
+  FormsModule,
+  DynamicComponentDefinition,
+  gettext
+} from '@c8y/ngx-components';
+
+@NgModule({
+  declarations: [WidgetPluginComponent, WidgetPluginConfig],
+  entryComponents: [WidgetPluginComponent, WidgetPluginConfig],
+  imports: [CommonModule, FormsModule],
+  exports: [],
+  providers: [
+    {
+      provide: HOOK_COMPONENTS,
+      multi: true,
+      useValue: [
+        {
+          id: 'angular.widget.plugin',
+          label: gettext('Extended Alarm Widget'),
+          description: 'Display and comment alarms',
+          component: WidgetPluginComponent,
+          previewImage: 'c8y-widget-preview-img/widget-plugin-pr.png',
+          configComponent: WidgetPluginConfig
+        }
+      ] as DynamicComponentDefinition[]
+    }
+  ]
+})
+export class WidgetPluginModule {}
